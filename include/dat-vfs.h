2ab9a751db7260f9e07d1efa8bfc2be4d366d331
Jtljac <jtljac2@gmail.com>
2023-06-30 03:56:16 +0100

    Move VFS implementations into cpp file

--- include/dat-vfs.h
+++ include/dat-vfs.h
@@ -27,32 +27,15 @@ namespace DVFS {
         /**
          * Create a root node
          */
-        DatVFS() {
-            folders["."] = this;
-            // Lacking a parent, set the parent to itself
-            folders[".."] = this;
-        }
+        DatVFS();
 
         /**
          * Create a folder node
          * @param parent The parent of this folder node
          */
-        DatVFS(DatVFS* parent) {
-            folders["."] = this;
-            folders[".."] = parent;
-        }
+        explicit DatVFS(DatVFS* parent);
 
-        ~DatVFS() {
-            for (auto& folder: folders) {
-                delete folder.second;
-            }
-            folders.clear();
-
-            for (auto& file: files) {
-                delete file.second;
-            }
-            files.clear();
-        }
+        ~DatVFS();
 
         // Folder management
         /**
@@ -61,26 +44,7 @@ namespace DVFS {
          * @param recursive Whether to create parent directories as needed
          * @return The newly created node, or nullptr if the folder couldn't be created
          */
-        DatVFS* createFolder(const DatPath& path, bool recursive = false) {
-            if (path.depth() > 1) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) {
-                    if (!recursive) return nullptr;
-
-                    // Call create folder for the validation
-                    folder = createFolder(path.getRoot());
-                    if (folder == nullptr) return nullptr;
-                }
-
-                return folder->createFolder(path.increment());
-            }
-
-            if (exists(path)) return nullptr;
-
-            DatVFS* folder = new DatVFS(this);
-            folders.emplace((std::string) path.getRoot(), folder);
-            return folder;
-        }
+        DatVFS* createFolder(const DatPath& path, bool recursive = false);
 
         // Mounting
         /**
@@ -90,25 +54,7 @@ namespace DVFS {
          * @param createFolders Whether to create parent directories as needed
          * @return true if successful
          */
-        bool mountFile(const DatPath& path, IDVFSFile* dvfsFile, bool createFolders = false) {
-            if (path.depth() > 1) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) {
-                    if (!createFolders) return false;
-
-                    // Call create folder for the validation
-                    folder = createFolder(path.getRoot());
-                    if (folder == nullptr) return false;
-                }
-
-                return folder->mountFile(path.increment(), dvfsFile, createFolders);
-            }
-
-            if (exists(path)) return false;
-
-            files.emplace((std::string) path.getRoot(), dvfsFile);
-            return true;
-        }
+        bool mountFile(const DatPath& path, IDVFSFile* dvfsFile, bool createFolders = false);
 
         /**
          * Mount multiple files on the VFS using an inserter
@@ -117,30 +63,7 @@ namespace DVFS {
          * @param createFolders Whether to create parent folders for the basePath and any paths for files being mounted
          * @return the number of files mounted
          */
-        int mountFiles(const DatPath& basePath, const IDVFSFileInserter& inserter, bool createFolders = false) {
-            if (basePath.depth() > 0) {
-                DatVFS* folder = getFolder(basePath.getRoot());
-                if (folder == nullptr) {
-                    if (!createFolders) return 0;
-
-                    // Call create folder for the validation
-                    folder = createFolder(basePath.getRoot());
-                    if (folder == nullptr) return 0;
-                }
-
-                return folder->mountFiles(basePath.increment(), inserter, createFolders);
-            }
-
-            int count = 0;
-            for (const auto& [path, idvfsFile]: inserter.getAllFiles()) {
-                if (mountFile(path, idvfsFile, createFolders)) ++count;
-                else {
-                    inserter.handleInsertFailure(path, idvfsFile);
-                }
-            }
-
-            return count;
-        }
+        int mountFiles(const DatPath& basePath, const IDVFSFileInserter& inserter, bool createFolders = false);
 
         // Unmount
         /**
@@ -148,42 +71,14 @@ namespace DVFS {
          * @param path The path to the file to unmount
          * @return True if successful
          */
-        bool unmountFile(const DatPath& path) {
-            if (path.depth() > 1) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return false;
-
-                return folder->unmountFile(path.increment());
-            }
-
-            IDVFSFile* idvfsFile = getFile(path);
-
-            if (idvfsFile == nullptr) return false;
-
-            files.erase((std::string) path);
-            delete idvfsFile;
-            return true;
-        }
+        bool unmountFile(const DatPath& path);
 
         /**
          * Remove a folder from the VFS, deleting it and all files and folders contained within
          * @param path The path to the folder to remove
          * @return True if successful
          */
-        bool removeFolder(const DatPath& path) {
-            DatVFS* folder = getFolder(path);
-            if (folder == nullptr) return false;
-
-            if (path.depth() > 1) {
-                return folder->removeFolder(path.increment());
-            }
-
-            folders.erase((std::string) path);
-
-            // File and subfolder deletion is handled by the destructor
-            delete folder;
-            return true;
-        }
+        bool removeFolder(const DatPath& path);
 
         // File Access
         /**
@@ -191,34 +86,14 @@ namespace DVFS {
          * @param path The path to the file
          * @return A pointer to the file, or nullptr if the file doesn't exist
          */
-        IDVFSFile* getFile(const DatPath& path) {
-            if (path.depth() > 1) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return nullptr;
-
-                return folder->getFile(path.increment());
-            }
-
-            auto it = files.find((std::string) path);
-            return it != files.end() ? it->second : nullptr;
-        }
+        IDVFSFile* getFile(const DatPath& path) const;
 
         /**
          * Get a folder inside the VFS
          * @param path The path to the folder
          * @return A pointer to the folder, or nullptr if the folder doesn't exist
          */
-        DatVFS* getFolder(const DatPath& path) {
-            if (path.depth() > 1) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return nullptr;
-
-                return folder->getFolder(path.increment());
-            }
-
-            auto it = folders.find((std::string) path);
-            return it != folders.end() ? it->second : nullptr;
-        }
+        DatVFS* getFolder(const DatPath& path) const;
 
         // Util
         /**
@@ -226,77 +101,34 @@ namespace DVFS {
          * @param path The path to the file/folder
          * @return positive if a file, negative if a folder, 0 for doesn't exist
          */
-        int exists(const DatPath& path) {
-            if (path.depth() > 1) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return false;
-                else return folder->exists(path.increment());
-            }
-
-            // Avoids branching, assumes that there will never be a folder and a file with the same name
-            return files.count((std::string) path.getRoot()) + (folders.count((std::string) path.getRoot()) * -1);
-        }
+        int exists(const DatPath& path) const;
 
         /**
          * Check if the given folder is empty
          * @param path The path to the folder
          * @return true if the given folder contains no files or folders
          */
-         bool empty(const DatPath& path = DatPath()) {
-            if (path.depth() > 0) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return false;
-                else return folder->empty(path.increment());
-            }
-
-            return folders.empty() && files.empty();
-         }
+         bool empty(const DatPath& path = DatPath()) const;
 
          /**
           * Check if this folder is the root of the VFS
           * @return true if this folder is the roof of the VFS
           */
-         bool isRoot(const std::filesystem::path& test) {
-             return getFolder("..") == this;
-         }
+         bool isRoot(const std::filesystem::path& test) const;
 
         /**
          * List the files in a directory
          * @param path The path to the folder to list (empty for the current folder)
          * @return A vector containing the names of all the files at the path
          */
-        std::vector<std::string> listFiles(const DatPath& path = DatPath()) {
-            if (path.depth() > 0) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return {};
-                else return folder->listFiles(path.increment());
-            }
-
-            std::vector<std::string> fileNames;
-            fileNames.reserve(files.size());
-            std::transform(files.begin(), files.end(), std::back_inserter(fileNames), [](const auto& pair){return pair.first;});
-
-            return fileNames;
-        }
+        std::vector<std::string> listFiles(const DatPath& path = DatPath()) const;
 
         /**
          * List the folders in a directory
          * @param path The path to the folder to list (empty for the current folder)
          * @return A vector containing the names of all the folders at the path
          */
-        std::vector<std::string> listFolders(const DatPath& path = DatPath()) {
-            if (path.depth() > 0) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return {};
-                else return folder->listFiles(path.increment());
-            }
-
-            std::vector<std::string> folderNames;
-            folderNames.reserve(folders.size());
-            std::transform(folders.begin(), folders.end(), std::back_inserter(folderNames), [](const auto& pair){return pair.first;});
-
-            return folderNames;
-        }
+        std::vector<std::string> listFolders(const DatPath& path = DatPath()) const;
 
         /**
          * Remove folders from the given directory if they're empty
@@ -304,31 +136,7 @@ namespace DVFS {
          * @param recursive Whether to also prune sub directories of the given directory
          * @return The number of folders deleted
          */
-        int prune(const DatPath& path = DatPath(), bool recursive = false) {
-            if (path.depth() > 0) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return {};
-                else return folder->prune(path.increment(), recursive);
-            }
-
-            int count = 0;
-            auto it = folders.begin();
-            while (it != folders.end()) {
-                DatVFS* folder = it->second;
-
-                // Do recursive first, so we can prune a folder that becomes empty after pruning
-                if (recursive) {
-                    count += folder->prune(path, recursive);
-                }
-
-                if (folder->empty()) {
-                    folders.erase(it++);
-                    ++count;
-                }
-            }
-
-            return count;
-        }
+        int prune(const DatPath& path = DatPath(), bool recursive = false);
 
         // Count Files (recursive, filter)
         /**
@@ -342,25 +150,7 @@ namespace DVFS {
          */
         int countFiles(const DatPath& path = DatPath(),
                        bool recursive = false,
-                       const std::function<bool(const std::string&, IDVFSFile*)>& predicate = [](const std::string&, IDVFSFile*){return true;}) {
-            if (path.depth() > 0) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return {};
-                else return folder->countFiles(path.increment(), recursive, predicate);
-            }
-
-            int count = std::accumulate(files.begin(), files.end(), 0, [&predicate](int acc, const auto& pair){
-                return predicate(pair.first, pair.second) ? acc + 1 : acc;
-            });
-
-            if (recursive) {
-                count += std::accumulate(folders.begin(), folders.end(), 0, [&predicate, &path](int acc, const auto& pair) {
-                    return acc + pair.second->countFiles(path, true, predicate);
-                });
-            };
-
-            return count;
-        }
+                       const std::function<bool(const std::string&, IDVFSFile*)>& predicate = [](const std::string&, IDVFSFile*){return true;}) const;
 
         /**
          * Count the number of folders that match the filter in the given folder
@@ -375,60 +165,13 @@ namespace DVFS {
          */
         int countFolders(const DatPath& path = DatPath(),
                        bool recursive = false,
-                       const std::function<bool(const std::string&, DatVFS*)>& predicate = [](const std::string&, DatVFS*){return true;}) {
-            if (path.depth() > 0) {
-                DatVFS* folder = getFolder(path.getRoot());
-                if (folder == nullptr) return {};
-                else return folder->countFolders(path.increment(), recursive, predicate);
-            }
-
-            int count = std::accumulate(folders.begin(), folders.end(), 0, [&path, recursive, &predicate](int acc, const auto& pair){
-                if (recursive) acc += pair.second->countFolders(path, recursive, predicate);
-                return predicate(pair.first, pair.second) ? acc + 1 : acc;
-            });
-
-            return count;
-        }
+                       const std::function<bool(const std::string&, DatVFS*)>& predicate = [](const std::string&, DatVFS*){return true;}) const;
 
         /**
          * Generate a string displaying the structure of the VFS
          * @param depth The current depth, used for calculating how to display the file/folder in the tree
          * @return A string representing the structure of the VFS
          */
-        std::string tree(const std::string& prefix = "") const {
-            std::stringstream stream;
-
-            bool noFiles = files.empty();
-            {
-                auto it = folders.begin();
-                while (it != folders.end()) {
-                    const std::string& name = it->first;
-                    const DatVFS* folder = it->second;
-                    bool end = std::next(it) == folders.end() && noFiles;
-
-                    stream << prefix << (end ? "└── " : "├── ") << name << std::endl;
-
-                    if (name == "." || name == "..") {
-                        ++it;
-                        continue;
-                    }
-
-                    stream << folder->tree(prefix + (end ? "    " : "│   "));
-                    ++it;
-                }
-            }
-
-            if (!noFiles) {
-                auto it = files.begin();
-                while (it != files.end()) {
-                    const std::string& name = it->first;
-                    bool end = std::next(it) == files.end();
-                    stream << prefix << (end ? "└── " : "├── ") << name << std::endl;
-                    ++it;
-                }
-            }
-
-            return stream.str();
-        }
+        std::string tree(const std::string& prefix = "") const;
     };
 }
--- source/dat-vfs.cpp
+++ source/dat-vfs.cpp
@@ -3,3 +3,300 @@
 //
 
 # include "../include/dat-vfs.h"
+
+DVFS::DatVFS::DatVFS() {
+    folders["."] = this;
+    // Lacking a parent, set the parent to itself
+    folders[".."] = this;
+}
+
+DVFS::DatVFS::DatVFS(DVFS::DatVFS* parent) {
+    folders["."] = this;
+    folders[".."] = parent;
+}
+
+DVFS::DatVFS::~DatVFS() {
+    for (auto& folder: folders) {
+        delete folder.second;
+    }
+    folders.clear();
+
+    for (auto& file: files) {
+        delete file.second;
+    }
+    files.clear();
+}
+
+DVFS::DatVFS* DVFS::DatVFS::createFolder(const DVFS::DatPath& path, bool recursive) {
+    if (path.depth() > 1) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) {
+            if (!recursive) return nullptr;
+
+            // Call create folder for the validation
+            folder = createFolder(path.getRoot());
+            if (folder == nullptr) return nullptr;
+        }
+
+        return folder->createFolder(path.increment());
+    }
+
+    if (exists(path)) return nullptr;
+
+    DatVFS* folder = new DatVFS(this);
+    folders.emplace((std::string) path.getRoot(), folder);
+    return folder;
+}
+
+bool DVFS::DatVFS::mountFile(const DVFS::DatPath& path, DVFS::IDVFSFile* dvfsFile, bool createFolders) {
+    if (path.depth() > 1) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) {
+            if (!createFolders) return false;
+
+            // Call create folder for the validation
+            folder = createFolder(path.getRoot());
+            if (folder == nullptr) return false;
+        }
+
+        return folder->mountFile(path.increment(), dvfsFile, createFolders);
+    }
+
+    if (exists(path)) return false;
+
+    files.emplace((std::string) path.getRoot(), dvfsFile);
+    return true;
+}
+
+int DVFS::DatVFS::mountFiles(const DVFS::DatPath& basePath, const DVFS::IDVFSFileInserter& inserter, bool createFolders) {
+    if (basePath.depth() > 0) {
+        DatVFS* folder = getFolder(basePath.getRoot());
+        if (folder == nullptr) {
+            if (!createFolders) return 0;
+
+            // Call create folder for the validation
+            folder = createFolder(basePath.getRoot());
+            if (folder == nullptr) return 0;
+        }
+
+        return folder->mountFiles(basePath.increment(), inserter, createFolders);
+    }
+
+    int count = 0;
+    for (const auto& [path, idvfsFile]: inserter.getAllFiles()) {
+        if (mountFile(path, idvfsFile, createFolders)) ++count;
+        else {
+            inserter.handleInsertFailure(path, idvfsFile);
+        }
+    }
+
+    return count;
+}
+
+bool DVFS::DatVFS::unmountFile(const DVFS::DatPath& path) {
+    if (path.depth() > 1) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return false;
+
+        return folder->unmountFile(path.increment());
+    }
+
+    IDVFSFile* idvfsFile = getFile(path);
+
+    if (idvfsFile == nullptr) return false;
+
+    files.erase((std::string) path);
+    delete idvfsFile;
+    return true;
+}
+
+bool DVFS::DatVFS::removeFolder(const DVFS::DatPath& path) {
+    DatVFS* folder = getFolder(path);
+    if (folder == nullptr) return false;
+
+    if (path.depth() > 1) {
+        return folder->removeFolder(path.increment());
+    }
+
+    folders.erase((std::string) path);
+
+    // File and subfolder deletion is handled by the destructor
+    delete folder;
+    return true;
+}
+
+DVFS::IDVFSFile* DVFS::DatVFS::getFile(const DVFS::DatPath& path) const {
+    if (path.depth() > 1) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return nullptr;
+
+        return folder->getFile(path.increment());
+    }
+
+    auto it = files.find((std::string) path);
+    return it != files.end() ? it->second : nullptr;
+}
+
+DVFS::DatVFS* DVFS::DatVFS::getFolder(const DVFS::DatPath& path) const {
+    if (path.depth() > 1) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return nullptr;
+
+        return folder->getFolder(path.increment());
+    }
+
+    auto it = folders.find((std::string) path);
+    return it != folders.end() ? it->second : nullptr;
+}
+
+int DVFS::DatVFS::exists(const DVFS::DatPath& path) const {
+    if (path.depth() > 1) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return false;
+        else return folder->exists(path.increment());
+    }
+
+    // Avoids branching, assumes that there will never be a folder and a file with the same name
+    return files.count((std::string) path.getRoot()) + (folders.count((std::string) path.getRoot()) * -1);
+}
+
+bool DVFS::DatVFS::empty(const DVFS::DatPath& path) const {
+    if (path.depth() > 0) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return false;
+        else return folder->empty(path.increment());
+    }
+
+    return folders.empty() && files.empty();
+}
+
+bool DVFS::DatVFS::isRoot(const std::filesystem::path& test) const {
+    return getFolder("..") == this;
+}
+
+std::vector<std::string> DVFS::DatVFS::listFiles(const DVFS::DatPath& path) const {
+    if (path.depth() > 0) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return {};
+        else return folder->listFiles(path.increment());
+    }
+
+    std::vector<std::string> fileNames;
+    fileNames.reserve(files.size());
+    std::transform(files.begin(), files.end(), std::back_inserter(fileNames), [](const auto& pair){return pair.first;});
+
+    return fileNames;
+}
+
+std::vector<std::string> DVFS::DatVFS::listFolders(const DVFS::DatPath& path) const {
+    if (path.depth() > 0) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return {};
+        else return folder->listFiles(path.increment());
+    }
+
+    std::vector<std::string> folderNames;
+    folderNames.reserve(folders.size());
+    std::transform(folders.begin(), folders.end(), std::back_inserter(folderNames), [](const auto& pair){return pair.first;});
+
+    return folderNames;
+}
+
+int DVFS::DatVFS::prune(const DVFS::DatPath& path, bool recursive) {
+    if (path.depth() > 0) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return {};
+        else return folder->prune(path.increment(), recursive);
+    }
+
+    int count = 0;
+    auto it = folders.begin();
+    while (it != folders.end()) {
+        DatVFS* folder = it->second;
+
+        // Do recursive first, so we can prune a folder that becomes empty after pruning
+        if (recursive) {
+            count += folder->prune(path, recursive);
+        }
+
+        if (folder->empty()) {
+            folders.erase(it++);
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int DVFS::DatVFS::countFiles(const DVFS::DatPath& path, bool recursive,
+                             const std::function<bool(const std::string&, IDVFSFile*)>& predicate) const {
+    if (path.depth() > 0) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return {};
+        else return folder->countFiles(path.increment(), recursive, predicate);
+    }
+
+    int count = std::accumulate(files.begin(), files.end(), 0, [&predicate](int acc, const auto& pair){
+        return predicate(pair.first, pair.second) ? acc + 1 : acc;
+    });
+
+    if (recursive) {
+        count += std::accumulate(folders.begin(), folders.end(), 0, [&predicate, &path](int acc, const auto& pair) {
+            return acc + pair.second->countFiles(path, true, predicate);
+        });
+    };
+
+    return count;
+}
+
+int DVFS::DatVFS::countFolders(const DVFS::DatPath& path, bool recursive,
+                               const std::function<bool(const std::string&, DatVFS*)>& predicate) const {
+    if (path.depth() > 0) {
+        DatVFS* folder = getFolder(path.getRoot());
+        if (folder == nullptr) return {};
+        else return folder->countFolders(path.increment(), recursive, predicate);
+    }
+
+    int count = std::accumulate(folders.begin(), folders.end(), 0, [&path, recursive, &predicate](int acc, const auto& pair){
+        if (recursive) acc += pair.second->countFolders(path, recursive, predicate);
+        return predicate(pair.first, pair.second) ? acc + 1 : acc;
+    });
+
+    return count;
+}
+
+std::string DVFS::DatVFS::tree(const std::string& prefix) const {
+    std::stringstream stream;
+
+    bool noFiles = files.empty();
+    {
+        auto it = folders.begin();
+        while (it != folders.end()) {
+            const std::string& name = it->first;
+            const DatVFS* folder = it->second;
+            bool end = std::next(it) == folders.end() && noFiles;
+
+            stream << prefix << (end ? "└── " : "├── ") << name << std::endl;
+
+            if (name == "." || name == "..") {
+                ++it;
+                continue;
+            }
+
+            stream << folder->tree(prefix + (end ? "    " : "│   "));
+            ++it;
+        }
+    }
+
+    if (!noFiles) {
+        auto it = files.begin();
+        while (it != files.end()) {
+            const std::string& name = it->first;
+            bool end = std::next(it) == files.end();
+            stream << prefix << (end ? "└── " : "├── ") << name << std::endl;
+            ++it;
+        }
+    }
+
+    return stream.str();
+}
